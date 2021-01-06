7add29f206a9d9d73b9e2e58f55c6e4bc7a6d578
NoobMaster <bk201_perez@yahoo.com>
2021-01-06 13:06:23 +0800

    yikes

--- public/js/app.js
+++ public/js/app.js
@@ -49886,8 +49886,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\admin\Documents\GitHub\iuris\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\admin\Documents\GitHub\iuris\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\admin\Documents\GitHub\TIPDBMS\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\admin\Documents\GitHub\TIPDBMS\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
